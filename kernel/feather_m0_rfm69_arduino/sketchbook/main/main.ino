9f4206fb75b35a3d7f40faf7efa834459868aca7
rafael.romanotero-at-570034927297 <rafael.romanotero@teejlab.com>
2020-12-06 21:54:48 -0800

    it's printing someting

--- kernel/feather_m0_rfm69_arduino/sketchbook/main/main.ino
+++ kernel/feather_m0_rfm69_arduino/sketchbook/main/main.ino
@@ -20,18 +20,12 @@
 **/
 #include "main.h"
 
-extern tPioPin led_pin;         //Defined as part of the HAL (in HAL IO)
-extern tSerialPort serial_usb;
-
 tPioPin led_pin_onboard;
 tPioPin led_pin_r;
 tPioPin led_pin_g;
 tPioPin led_pin_b;
 
 void sample_kthread(void){
-
-  //while(!hal_io_serial_is_ready(&serial_usb)); /// <<--- WAIT FOR USER
-
   //
   //Each pin has a 2.2K resistor
   //
@@ -40,20 +34,30 @@ void sample_kthread(void){
   pio_create_pin(&led_pin_g, PioA, 16, PioOutput);
   pio_create_pin(&led_pin_b, PioA, 19, PioOutput);
 
+  //Light sensor
+  tAdcChannel light_sensor_adc;
+  hal_io_adc_create_channel(&light_sensor_adc, AdcA, IoPoll);
+
   while(true){
-    pio_write(&led_pin, !pio_read(&led_pin));
+    pio_write(&led_pin_onboard, !pio_read(&led_pin_onboard));
     pio_write(&led_pin_r, true);
     pio_write(&led_pin_g, false);
     pio_write(&led_pin_b, false);
 
+    kprintf_debug("\n\rLight Sensor = %d \n\r", hal_io_adc_read(&light_sensor_adc));
+
     hal_cpu_delay(1000);
 
-    pio_write(&led_pin, !pio_read(&led_pin));
+    pio_write(&led_pin_onboard, !pio_read(&led_pin_onboard));
     pio_write(&led_pin_r, true);
     pio_write(&led_pin_g, true);
     pio_write(&led_pin_b, true);
 
+    kprintf_debug("\n\rLight Sensor = %d \n\r", hal_io_adc_read(&light_sensor_adc));
+
     hal_cpu_delay(1000);
+
+
   }
 
 }
