a67026fe04bebc9ebba9285544a0fb43f10cb22e
rafael.romanotero-at-570034927297 <rafael.romanotero@teejlab.com>
2020-12-20 03:54:50 -0800

    motors first commit

---
+++ kernel/feather_m0_rfm69_arduino/src/main.c
@@ -0,0 +1,48 @@
+/**
+*   This file is part of IcedCoffeeOS
+*   (https://github.com/rromanotero/IcedCoffeeOS).
+*
+*   Copyright (c) 2020 Rafael Roman Otero.
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*
+**/
+#include "hal.h"
+
+extern tPioPin led_pin;
+volatile int32_t light_intensity = 0;
+
+void motor_kthread(void){
+
+
+  while(true){
+      kprintf_debug("Motor thread");
+      hal_cpu_delay(1000);
+  }
+
+}
+
+
+
+void ARDUINO_KERNEL_MAIN() {
+  system_init();
+
+  scheduler_thread_create( motor_kthread, "motor_kthread", 512, ProcQueueReadyRealTime );
+
+  while(true);
+
+  //Exit so we don't
+  //loop over and over
+  exit(0);
+}
